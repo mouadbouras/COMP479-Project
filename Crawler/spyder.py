e12a6b2ecd8a8d8dd11dee93502112f34ab2138b
Sean Canlas <igotnocans@users.noreply.github.com>
2017-11-25 22:47:40 -0500

    What's git?
    I upload my code yallah

--- Crawler/spyder.py
+++ Crawler/spyder.py
@@ -4,9 +4,12 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy import signals
 import io
 import re
 import html2text
+import os
 
 class Spyder(scrapy.Spider):
     name = "Spyder"
@@ -22,47 +25,68 @@ class Spyder(scrapy.Spider):
             "http://www.cupfa.org/",
             "http://www.concordia.ca/artsci/students/associations.html"
         ]
-        
+
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
+
 
     # def start_requests(self):
     #     for url in urls:
     #         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        h = html2text.HTML2Text()
         hxs = Selector(response)
         title = hxs.xpath('//title').extract()
         body = hxs.xpath('//body').extract()
-
-        # noHTMLRegex = re.compile('(<.*?>)|<.*?>.*?</script>|<.*?>.*?</style>|&.*?;|<.*?>.*?</iframe>|<!--.*?-->')
-        # body[0] = re.sub(noHTMLRegex, ' ', body[0])
-        # title[0] = re.sub(noHTMLRegex, ' ', title[0])
-
-        h = html2text.HTML2Text()
-        h.ignore_links = True
-        body[0] = h.handle(body[0])
         title[0] = h.handle(title[0])
+        title[0] = title[0].strip("\r\n")
 
-        extracted = title[0] + '\n' + body[0]
-        fileName = str(self.CORPUS) + ".txt"
+        if(title[0] != "Page not found"):
+            fileName = "../Dumps/"+str(self.CORPUS) + ".txt"
+            if(os.path.exists(fileName)):
+                filesize = os.stat(fileName).st_size
+                if(filesize > 10000):
+                    with io.open(fileName, "a", encoding="utf-8") as f:
+                        f.write("</documents>")
+                    self.CORPUS += 1
+                    fileName = "../Dumps/"+str(self.CORPUS) + ".txt"
+                    with io.open(fileName, "a", encoding="utf-8") as f:
+                        f.write("<documents>")
+                    fileSize = 0
+            else:
+                with io.open(fileName, "a", encoding="utf-8") as f:
+                    f.write("<documents>")
+                filesize = 0
 
+            # noHTMLRegex = re.compile('(<.*?>)|<.*?>.*?</script>|<.*?>.*?</style>|&.*?;|<.*?>.*?</iframe>|<!--.*?-->')
+            # body[0] = re.sub(noHTMLRegex, ' ', body[0])
+            # title[0] = re.sub(noHTMLRegex, ' ', title[0])
 
-        # if()
-        #     self.CORPUS += 1
+            h.ignore_links = True
+            body[0] = h.handle(body[0])
 
-        corpusTitle = '<document id="' + str(self.ID) + '" url= "' + response.url + '">'
-        self.ID += 1
-        corpusEnd = "</document>"
+            extracted = title[0] + '\n' + body[0]
 
+            corpusTitle = '<document id="' + str(self.ID) + '" url= "' + response.url + '">'
+            self.ID += 1
+            corpusEnd = "</document>"
+
+            if(title[0] != "Page not found"):
+                with io.open(fileName, "a", encoding="utf-8") as f:
+                    f.write(corpusTitle + title[0] + body[0] + corpusEnd)
+
+            urls = hxs.xpath('//a/@href').extract()
+
+            for url in urls:
+                if(url not in self.URLS):
+                    if(self.COUNT < self.MAX):
+                        self.URLS.append(url)
+                        self.COUNT += 1
+
+            for url in self.URLS:
+                yield response.follow(url, self.parse)
+
+    def spider_closed(self, spider):
+        fileName = "../Dumps/"+str(self.CORPUS) + ".txt"
         with io.open(fileName, "a", encoding="utf-8") as f:
-            f.write(corpusTitle + title[0] + body[0] + corpusEnd)
-
-        urls = hxs.xpath('//a/@href').extract()
-
-        for url in urls:
-            if(url not in self.URLS):
-                if(self.COUNT < self.MAX):
-                    self.URLS.append(url)
-                    self.COUNT += 1
-
-        for url in self.URLS:
-            yield response.follow(url, self.parse)
+            f.write("</documents>")
