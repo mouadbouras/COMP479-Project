77e749ada9990bb8c90099b9a88bd390fc73c483
mouad bouras <br_mouad@live.fr>
2017-11-26 23:22:36 -0500

    fixed issue with rank by sentiment created new function for it

--- Indexer/ranker.py
+++ Indexer/ranker.py
@@ -19,13 +19,13 @@ class Ranker(object):
     # limits number of results to amount passed in topx 
     def do_ranking(query, result, files, topx):
         rankedresults = Ranker.rank_by_sentiment(query, result, files)
-        itemcnt = 0
-        for k in rankedresults:
-            if itemcnt >= topx:
-                rankedresults.pop(k)
-            itemcnt += 1
-
-        return rankedresults
+        # itemcnt = 0
+        # print(rankedresults)
+        # for k in rankedresults:
+        #     if itemcnt >= topx:
+        #         rankedresults.pop(k)
+        #     itemcnt += 1
+        return rankedresults[:topx]
 
     def open_json(index_file):
         data = Tools.loadDictionary(index_file)
@@ -34,7 +34,7 @@ class Ranker(object):
     # rank by sentiment taking query, dict{docId, tf-idf}, files
     # if sentiment(query) >= 0 : rank docs from positive to negative
     # else : rank docs from negative to positive
-    def rank_by_sentiment(query, docweights, files):
+    def rank_by_sentiment_OLD(query, docweights, files):
         querysentiment = Tools.sentiment(query)
         docsentiments = {}
         ordereddocs = OrderedDict()
@@ -42,10 +42,35 @@ class Ranker(object):
             doc = Tools.getDocById(k, files)
             docsentiments[k] = Tools.sentiment(doc)
         if querysentiment >= 0:
-            ordereddocs = sorted(docsentiments, key=itemgetter(1), reverse=True)
+            ordereddocs = sorted(docsentiments,  reverse=True)
         else:
-            ordereddocs = sorted(docsentiments, key=itemgetter(1))
+            ordereddocs = sorted(docsentiments, )
         return ordereddocs
+
+
+    def rank_by_sentiment(query, query_result, files):
+        print(query_result)
+        pos = []
+        neg = []
+        querysentiment = Tools.sentiment(query)
+
+        for doc in sorted(query_result, key=query_result.get,  reverse=True): 
+            document = Tools.getDocById(doc, files)
+            docsentiment = Tools.sentiment(document)
+            # print(str(doc) + "|" + str(docsentiment))
+            if docsentiment >= 0:
+                pos.append(doc)
+            else :
+                neg.append(doc)
+        # print(pos)
+        # print(neg)
+        if querysentiment >= 0:
+            pos.extend(neg)
+            return pos
+        else:
+            neg.extend(pos)
+            return neg
+
         
     # docs = only docs returned by query result, not everything in the index
     # recalculates tf for every doc, not sure whether to adjust
